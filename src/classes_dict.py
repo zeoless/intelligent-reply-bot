aef968b39d7c27b3b27a6f5629f86fe20df36ded
zeoless <125026834+zeoless@users.noreply.github.com>
2022-02-23 14:02:30 +0000

    CSS fix

--- src/classes_dict.py
+++ src/classes_dict.py
@@ -2,4 +2,4 @@
 classes_dict = {}
 
 classes_dict["greeting"] = {}
-classes_dict["greeting"]["pattern"] = ["hi", "hi there", "hello", "h
+classes_dict["greeting"]["pattern"] = ["hi", "hi there", "hello", "hey", "good afternoon", "good mo
