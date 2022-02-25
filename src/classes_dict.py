0234305400ba6e7ee841e626164b79b11de6ca0c
zeoless <125026834+zeoless@users.noreply.github.com>
2022-02-25 09:46:19 +0000

    revert some changes

--- src/classes_dict.py
+++ src/classes_dict.py
@@ -2,4 +2,5 @@
 classes_dict = {}
 
 classes_dict["greeting"] = {}
-classes_dict["greeting"]["pattern"] = ["hi", "hi there", "hello", "hey", "good afternoon", "good morning", "good evening", "good day"
+classes_dict["greeting"]["pattern"] = ["hi", "hi there", "hello", "hey", "good afternoon", "good morning", "good evening", "good day"]
+classes_dict["greeting"]["response"]
