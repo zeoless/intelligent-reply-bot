3573e4fc00ddd62b8f5f103ab0f30f2610dd4c8a
zeoless <125026834+zeoless@users.noreply.github.com>
2022-02-25 10:07:50 +0000

    Missing parenthesis

--- src/classes_dict.py
+++ src/classes_dict.py
@@ -3,4 +3,6 @@ classes_dict = {}
 
 classes_dict["greeting"] = {}
 classes_dict["greeting"]["pattern"] = ["hi", "hi there", "hello", "hey", "good afternoon", "good morning", "good evening", "good day"]
-classes_dict["greeting"]["response"]
+classes_dict["greeting"]["response"] = ["hi", "hey", "hello"]
+
+classe
