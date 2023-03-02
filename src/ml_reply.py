decea1c31133e859e09e440734f876debacf9311
zeoless <125026834+zeoless@users.noreply.github.com>
2023-03-02 22:07:32 +0000

    #34

--- src/ml_reply.py
+++ src/ml_reply.py
@@ -51,3 +51,42 @@ for some_doc in my_doc:
     pattern_words = some_doc[0]
     
     # create bag of words array
+    for some_word in my_words:
+        if some_word in pattern_words:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+output_row = list(output_empty)
+output_row[my_classes.index(some_doc[1])] = 1
+
+training.append([bag, output_row])
+
+# shuffle training data and put into array
+random.shuffle(training)
+training = np.array(training)
+
+# create train lists
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+
+# reset underlying graph data
+tf.reset_default_graph()
+
+# build nn model
+net = tflearn.input_data(shape = [None, len(train_x[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(train_y[0]), activation = 'softmax')
+net = tflearn.regression(net)
+
+# define model and setup tensorboard
+model = tflearn.DNN(net, tensorboard_dir = 'tflearn_logs')
+
+# train model
+# model.fit(train_x, train_y, n_epoch = 500, batch_size = 8, show_metric = True)
+# model.save('model_stupid.tflearn')
+# pickle.dump( {'my_words':my_words, 'my_classes':my_classes, 'train_x':train_x, 'train_y':train_y}, open( "training_data", "wb" ) )
+
+# load model
+model.load('./model_laura.tflearn')
